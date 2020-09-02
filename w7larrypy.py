a8ed253451ac041efcf6b32c04b33c249a19747c
Larry1524 <68308643+Larry1524@users.noreply.github.com>
2020-09-01 19:00:56 -0500

    Add files via upload

---
+++ w7larrypy.py
@@ -0,0 +1,59 @@
+#problem1
+import math
+r = input("what is the radius")
+p = 22 / 7
+print(p * (r * r))
+
+#Problem2
+number = input("Please enter a number")
+if number in range(1, 10):
+      print("ITS IN RANGE!")
+else:
+      print("ITS NOT IN RANGE!")
+
+#problem3
+import math
+print(5 * 2 * 7 * -1)
+
+#problem4
+import random
+print("unique elements for 1,3,3,3,6,2,3,5")
+num1 = random.randint(1, 6)
+print("your unique number: ", num1)
+
+#problem5
+wn = turtle.Screen()
+z = turtle.Turtle()
+z.color("blue")
+z.forward(80)
+z.left(90)
+z.forward(80)
+z.left(90)
+z.forward(80)
+z.left(90)
+z.forward(80)
+z.left(45)
+z.right(90)
+z.forward(10)
+z.left(135)
+z.forward(105)
+z.left(90)
+z.forward(100)
+z.left(90)
+z.forward(110)
+z.left(90)
+z.forward(100)
+z.left(90)
+z.forward(10)
+z.left(180)
+z.forward(10)
+z.left(45)
+z.forward(20)
+z.left(135)
+z.forward(140)
+z.left(90)
+z.forward(140)
+z.left(90)
+z.forward(140)
+z.left(90)
+z.forward(140)
